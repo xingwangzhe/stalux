362e74fcf62e62a23fbfe61bdf7e2ff643b1f5d8
王兴家 <xingwangzhe@outlook.com>
2025-03-30 19:22:22 +0800

    feat:测试测试一下啊

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -1,6 +1,6 @@
 export default defineNuxtConfig({
   compatibilityDate: '2024-11-01',
-  devtools: { enabled: true },
+  devtools: { enabled: false },
   components: true,
   modules: [
     '@nuxt/content',
---
+++ pages/archives.vue
@@ -0,0 +1,18 @@
+<template>
+  <div>
+    你好啊
+    <li  v-for="(item, index) in data"  :key="index">
+    <NuxtLink v-if="item.abbrlink"  :to="`/posts/${item.abbrlink}`">{{ item.title }}</NuxtLink>
+</li>
+  </div>
+</template>
+
+<script lang="ts" setup>
+  const { data , error} =await useAsyncData('posts',()=>{
+  return queryCollection('posts').all();
+})
+</script>
+
+<style>
+
+</style>
