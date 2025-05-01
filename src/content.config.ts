c3a5c769464f603dc4befa5ec264344a7b1603cb
王兴家 <xingwangzhe@outlook.com>
2025-05-01 18:37:12 +0800

    feat:分类实现

--- src/content.config.ts
+++ src/content.config.ts
@@ -1,6 +1,13 @@
 import { glob } from 'astro/loaders';
 import { defineCollection, z } from 'astro:content';
 
+// 定义分类的递归结构
+const categorySchema:any = z.lazy(() => z.object({
+  name: z.string(),
+  next: z.array(categorySchema).optional()
+}));
+
+
 const posts = defineCollection({
 	loader: glob({ base: './src/content/posts', pattern: '**/*.{md,mdx}' }),
 	schema: z.object({
@@ -10,7 +17,8 @@ const posts = defineCollection({
 		// 更新时间可选，接受日期字符串或 Date 对象
 		updated: z.union([z.string(), z.coerce.date()]).optional(),
 		tags: z.array(z.string()).optional(),
-		categories: z.array(z.string()).optional(),
+		// 更新分类为树形结构
+		categories: z.array(categorySchema).optional(),
 		abbrlink: z.union([z.string(), z.number()]).optional(),
 	}),
 });
