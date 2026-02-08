9b040cf3ba30c3448d96675f1984a0b3473081a6
王兴家 <xingwangzhe@outlook.com>
2026-02-08 09:19:46 +0800

    asd

--- astro.config.mjs
+++ astro.config.mjs
@@ -71,6 +71,12 @@ export default defineConfig({
     }),
   ],
   vite: {
+    define: {
+      // Vue feature flags for Waline
+      __VUE_OPTIONS_API__: true,
+      __VUE_PROD_DEVTOOLS__: false,
+      __VUE_PROD_HYDRATION_MISMATCH_DETAILS__: false,
+    },
     build: {
       cssMinify: "lightningcss",
       target: "es2022",
