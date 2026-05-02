44764a188bcc2e76ac146d1ed0f9c08f11524880
王兴家 <xingwangzhe@outlook.com>
2026-05-02 08:45:38 +0800

    fmt

--- src/utils/remark-post-body.ts
+++ src/utils/remark-post-body.ts
@@ -1,5 +1,5 @@
-import { toString } from "mdast-util-to-string";
 import { countWords } from "alfaaz";
+import { toString } from "mdast-util-to-string";
 import { visit } from "unist-util-visit";
 
 function formatReadingTime(words: number, wordsPerMinute: number): string {
